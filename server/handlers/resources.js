5df4f87c4a532320e0293bcca65109a0c9169540
Gilles Perreymond <gperreymond@gmail.com>
2017-08-02 21:55:22 +0200

    glob for tiles

--- server/handlers/resources.js
+++ server/handlers/resources.js
@@ -1,28 +1,35 @@
+const path = require('path')
+const glob = require('glob')
+const eraro = require('eraro')({package: 'warboard'})
+const Boom = require('boom')
+
 module.exports = {
   auth: false,
   handler (request, reply) {
-    return reply({
-      tiles: {
-        RoomTileAir: 'http://localhost:3000/data/images/5x5-air.png',
-        RoomTileEarth: 'http://localhost:3000/data/images/5x5-earth.png',
-        RoomTileFire: 'http://localhost:3000/data/images/5x5-fire.png',
-        RoomTileNature: 'http://localhost:3000/data/images/5x5-nature.png',
-        RoomTileMagic: 'http://localhost:3000/data/images/5x5-magic.png',
-        RoomTileUndead: 'http://localhost:3000/data/images/5x5-undead.png',
-        RoomTileTech: 'http://localhost:3000/data/images/5x5-tech.png'
-      },
-      walls: {
-        Walls000: 'http://localhost:3000/data/images/walls-000.png',
-        Walls001: 'http://localhost:3000/data/images/walls-001.png',
-        Walls002: 'http://localhost:3000/data/images/walls-002.png',
-        Walls003: 'http://localhost:3000/data/images/walls-003.png'
-      },
-      textures: {
-        RoomDoorHorizontal: 'http://localhost:3000/data/images/door-h.png',
-        RoomDoorVertical: 'http://localhost:3000/data/images/door-v.png',
-        RoomWallHorizontal: 'http://localhost:3000/data/images/wall-h.png',
-        RoomWallVertical: 'http://localhost:3000/data/images/wall-v.png'
+    glob(path.resolve(__dirname, '../../data/images/tiles/*.*'), {}, (error, files) => {
+      if (error) {
+        return reply(Boom.badRequest(eraro('400', 'BAD_REQUEST', error)))
       }
+      let tiles = {}
+      files.map(file => {
+        let fname = path.basename(file, path.extname(file))
+        tiles[fname] = request.server.info.uri + '/data/images/tiles/' + path.basename(file)
+      })
+      return reply({
+        tiles,
+        walls: {
+          Walls000: 'http://localhost:3000/data/images/walls-000.png',
+          Walls001: 'http://localhost:3000/data/images/walls-001.png',
+          Walls002: 'http://localhost:3000/data/images/walls-002.png',
+          Walls003: 'http://localhost:3000/data/images/walls-003.png'
+        },
+        textures: {
+          RoomDoorHorizontal: 'http://localhost:3000/data/images/door-h.png',
+          RoomDoorVertical: 'http://localhost:3000/data/images/door-v.png',
+          RoomWallHorizontal: 'http://localhost:3000/data/images/wall-h.png',
+          RoomWallVertical: 'http://localhost:3000/data/images/wall-v.png'
+        }
+      })
     })
   }
 }
